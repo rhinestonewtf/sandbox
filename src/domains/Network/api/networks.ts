293f2b01a0fd73dc4f2370e52d3883c393271ecb
kopy-kat <kopp.konrad@gmail.com>
2024-06-09 19:15:43 +0100

    fix: switch to sepolia as default network

--- src/domains/Network/api/networks.ts
+++ src/domains/Network/api/networks.ts
@@ -2,25 +2,25 @@ import { Network } from "../Network";
 
 export const networks: Network[] = [
   {
-    name: "Polygon Mumbai",
+    name: "Sepolia",
     testnet: true,
     icon: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/info/logo.png",
     bundlerUrl: process.env.NEXT_PUBLIC_BUNDLER_URL_MUMBAI!,
-    rpcUrl: "https://rpc.ankr.com/polygon_mumbai",
-    zeroExBaseUrl: "",
-    id: 80001,
-    network: "Polygon Mumbai",
-    queryName: "mumbai",
-    nativeCurrency: { name: "Polygon MATIC", symbol: "MATIC", decimals: 18 },
+    rpcUrl: "https://rpc.ankr.com/eth_sepolia",
+    zeroExBaseUrl: "https://sepolia.api.0x.org",
+    id: 11_155_111,
+    network: "Sepolia",
+    queryName: "sepolia",
+    nativeCurrency: { name: "Sepolia Ether", symbol: "ETH", decimals: 18 },
     rpcUrls: {
       default: {
-        http: ["https://rpc.ankr.com/polygon_mumbai"],
+        http: ["https://rpc.ankr.com/eth_sepolia"],
       },
       public: {
-        http: ["https://rpc.ankr.com/polygon_mumbai"],
+        http: ["https://rpc.ankr.com/eth_sepolia"],
       },
     },
-    blockExplorerUrl: "https://mumbai.polygonscan.com",
-    explorerName: "PolygonScan",
+    blockExplorerUrl: "https://sepolia.etherscan.io",
+    explorerName: "Etherscan",
   },
 ];
